10c5e066df24ffe9b129ecbad1136ebccbc95b92
hu <337502348@qq.com>
2021-04-19 13:22:21 +0800

    尝试

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { ReduxAndRouter, b } from './common/redux'
+import { ReduxAndRouter, c } from './common/redux'
 import './App.css';
 
 const App: FC = () => (
