e36c336d7710c10185c42518b462c1779a6e9b15
hu <337502348@qq.com>
2021-04-19 13:37:39 +0800

    尝试

--- src/pages/ahooks/page-view.tsx
+++ src/pages/ahooks/page-view.tsx
@@ -3,11 +3,11 @@ import useFetchQueryVal  from '../../common/hooks/useFetchQueryVal';
 import Comps from './component/index'
 
 export default function Ahooks (){
-  // let val = useFetchQueryVal('type')
+  let val = useFetchQueryVal('type')
   // let Comp = Comps[val]
   return (
     <div>
-      {/* { Comp ? <Comp></Comp> : <></> } */}
+      { Comp ? <Comp></Comp> : <></> }
     </div>
   );
 };
